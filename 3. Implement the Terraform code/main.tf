a3bf1f7e6c75a5788b03e555ccb500a1b01fd3a5
Anoop Kumar <43013645+anoopkum@users.noreply.github.com>
2022-07-15 06:51:05 +0100

    Update main.tf

--- 3. Implement the Terraform code/main.tf
+++ 3. Implement the Terraform code/main.tf
@@ -1,36 +1,7 @@
-# Generate random resource group name
-resource "random_pet" "rg-name" {
-  prefix    = var.resource_group_name_prefix
-}
-
+# Generate resource group name
 resource "azurerm_resource_group" "rg" {
-  name      = random_pet.rg-name.id
+  name      = var.rg-name.id
   location  = var.resource_group_location
-}
-
-resource "random_id" "log_analytics_workspace_name_suffix" {
-    byte_length = 8
-}
-
-resource "azurerm_log_analytics_workspace" "test" {
-    # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
-    location            = var.log_analytics_workspace_location
-    resource_group_name = azurerm_resource_group.k8s.name
-    sku                 = var.log_analytics_workspace_sku
-}
-
-resource "azurerm_log_analytics_solution" "test" {
-    solution_name         = "ContainerInsights"
-    location              = azurerm_log_analytics_workspace.test.location
-    resource_group_name   = azurerm_resource_group.k8s.name
-    workspace_resource_id = azurerm_log_analytics_workspace.test.id
-    workspace_name        = azurerm_log_analytics_workspace.test.name
-
-    plan {
-        publisher = "Microsoft"
-        product   = "OMSGallery/ContainerInsights"
-    }
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
@@ -56,13 +27,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     service_principal {
         client_id     = var.aks_service_principal_app_id
         client_secret = var.aks_service_principal_client_secret
-    }
-
-    addon_profile {
-        oms_agent {
-        enabled                    = true
-        log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
-        }
     }
 
     network_profile {
